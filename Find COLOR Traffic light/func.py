a56a08e274f5a6682cf27756d223a1e53526ceb0
Jalilov <jalilovjavohir777@gmail.com>
2021-05-29 13:10:37 +0500

    add get musk function

--- Find COLOR Traffic light/func.py
+++ Find COLOR Traffic light/func.py
@@ -24,3 +24,8 @@ def get_img(name):
     img = cv2.blur(img,(12,12))
     gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     return gray,img
+
+def get_mask(img):
+  mask = np.zeros(img.shape,dtype=np.uint8)
+  mask = cv2.rectangle(mask,(180,180),(600,1400),color=(255,255,255),thickness=-1)
+  return mask
