36c1c82896d97adc8eb642752fc1c3b0761bf99d
Jalilov <jalilovjavohir777@gmail.com>
2021-05-29 13:53:20 +0500

    add find bbox img

--- Find COLOR Traffic light/func.py
+++ Find COLOR Traffic light/func.py
@@ -44,3 +44,32 @@ def filters_img(ROI_img,start_color,stop_color,kernal):
     opening = cv2.morphologyEx(img,cv2.MORPH_OPEN,kernal(20,20))
     dilation = cv2.dilate(opening,kernal(18,18))
     return dilation
+
+def find_color(img):
+    H,W = img.shape
+    h = H//3
+    red = img[:h,:]
+    yellow = img[h:h*2,:]
+    green = img[h*2:h*3,:]
+    
+    count_red = len(red[red == 0])
+    count_yellow = len(yellow[yellow == 0])
+    count_green = len(green[green == 0])
+    min_color_black = min(count_red,count_yellow,count_green)
+    color = ''
+    if count_red == min_color_black:
+        color = 'RED'
+    elif count_yellow == min_color_black:
+        color = 'YELLOW'
+    elif count_green == min_color_black:
+        color = 'GREEN'
+
+    return color
+
+def bbox_img(img_mask):
+    # contours,_ = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHANIN_APPROX_SIMPLE)
+    box = cv2.boundingRect(img_mask)
+    x,y,w,h = box
+    start_point = (x,y)
+    end_point = (x+w,y+h)
+    return start_point,end_point
